785bdaf47ffa7df8a19c73b0b90ad37539b66b32
Frederick Grose <fgrose@sugarlabs.org>
2024-08-20 16:27:52 -0700

    refactor(dmsquash-live-autooverlay): convert to a library module
    Rename create-overlay.sh to partition-lib.sh and delete files
      create-overlay-genrules.sh & 70dmsquash-live-autooverlay/module-setup.sh.
    Use data already gathered in dmsquash-live-root.sh.
    Remove unneeded parameter rd.live.overlay.nouserconfirmprompt.
    Call rd_live_check() as required before modifying the disk.
    
    Providing library code eases integration with dmsquash-live and opens
    the conceptual scope to all sorts of partition editing utilities to
    be added to enhance partitioning features.

---
+++ modules.d/70partition-lib/module-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check() {
+    require_binaries parted || return 1
+    return 255
+}
+
+installkernel() {
+    instmods btrfs ext4 fat xfs
+}
+
+install() {
+    inst_multiple awk blkid cat grep mkdir mount parted readlink rmdir tr umount
+    inst_multiple -o mkfs.btrfs mkfs.ext4 mkfs.fat mkfs.xfs
+    inst_simple "$moddir/partition-lib.sh" "/lib/partition-lib.sh"
+}
