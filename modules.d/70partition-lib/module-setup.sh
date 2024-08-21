5926c76a6b7e9e58659db23920e9daaf607dbce4
Frederick Grose <fgrose@sugarlabs.org>
2024-08-21 15:51:17 -0700

    refactor(partition-lib): use parted to gather partition table info
    Set locale and lock <device> for parted calls.
    Set 256 MiB as minimum size for a new persistence partition.
    Remove unneed dependencies on awk, cat, grep, readlink, & tr.

--- modules.d/70partition-lib/module-setup.sh
+++ modules.d/70partition-lib/module-setup.sh
@@ -10,7 +10,7 @@ installkernel() {
 }
 
 install() {
-    inst_multiple awk blkid cat grep mkdir mount parted readlink rmdir tr umount
+    inst_multiple blkid mkdir mount parted rmdir umount
     inst_multiple -o mkfs.btrfs mkfs.ext4 mkfs.fat mkfs.xfs
     inst_simple "$moddir/partition-lib.sh" "/lib/partition-lib.sh"
 }
