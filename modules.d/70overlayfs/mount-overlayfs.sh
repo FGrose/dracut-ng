5aaf8252074acd05c3d6013217387e6c2d99f687
Frederick Grose <fgrose@sugarlabs.org>
2024-09-02 11:59:46 -0700

    fix(overlayfs,dmsquash-live,livenet): apply mount flags on upper mount
    For OverlayFS mounts, apply any special flags on the overlay partition.
    Use the canonical persistence partition name, if available.
    Use the specified filesystem type in the mount call.

--- modules.d/70overlayfs/mount-overlayfs.sh
+++ modules.d/70overlayfs/mount-overlayfs.sh
@@ -5,12 +5,10 @@ command -v getarg > /dev/null || . /lib/dracut-lib.sh
 getargbool 0 rd.overlayfs -d rd.live.overlay.overlayfs && OverlayFS="yes"
 getargbool 0 rd.overlayfs.readonly -d rd.live.overlayfs.readonly && readonly_overlay="--readonly"
 
-ROOTFLAGS="$(getarg rootflags)"
-
 if [ -n "$OverlayFS" ]; then
     basedirs=lowerdir=${readonly_overlay:+/run/overlayfs-r:}/run/rootfsbase
 
     if ! strstr "$(cat /proc/mounts)" LiveOS_rootfs; then
-        mount -t overlay LiveOS_rootfs -o "$ROOTFLAGS,$basdirs",upperdir=/run/overlayfs,workdir=/run/ovlwork "$NEWROOT"
+        mount -t overlay LiveOS_rootfs -o "$basdirs",upperdir=/run/overlayfs,workdir=/run/ovlwork "$NEWROOT"
     fi
 fi
