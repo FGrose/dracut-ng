e7c7d03e9b407c0f8cd728e38d0964022e4dde8c
Frederick Grose <fgrose@sugarlabs.org>
2024-09-03 20:58:28 -0700

    refactor(mount-overlayfs): use ismounted() function and exit value logic
    Use the library function ismounted() with exit value logic to simplify
    the code.

--- modules.d/70overlayfs/mount-overlayfs.sh
+++ modules.d/70overlayfs/mount-overlayfs.sh
@@ -2,13 +2,12 @@
 
 command -v getarg > /dev/null || . /lib/dracut-lib.sh
 
-getargbool 0 rd.overlayfs -d rd.live.overlay.overlayfs && OverlayFS="yes"
-getargbool 0 rd.overlayfs.readonly -d rd.live.overlayfs.readonly && readonly_overlay="--readonly"
+getargbool 0 rd.overlayfs -d rd.live.overlay.overlayfs || return 0
 
-if [ -n "$OverlayFS" ]; then
+ismounted LiveOS_rootfs || {
+    getargbool 0 rd.overlayfs.readonly -d rd.live.overlayfs.readonly && readonly_overlay="--readonly"
+
     basedirs=lowerdir=${readonly_overlay:+/run/overlayfs-r:}/run/rootfsbase
 
-    if ! strstr "$(cat /proc/mounts)" LiveOS_rootfs; then
-        mount -t overlay LiveOS_rootfs -o "$basdirs",upperdir=/run/overlayfs,workdir=/run/ovlwork "$NEWROOT"
-    fi
-fi
+    mount -t overlay LiveOS_rootfs -o "$basdirs",upperdir=/run/overlayfs,workdir=/run/ovlwork "$NEWROOT"
+}
