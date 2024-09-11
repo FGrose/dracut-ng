81b4351a4b1916f83e7d56f247603b11c9ce2ca5
Frederick Grose <fgrose@sugarlabs.org>
2024-09-11 09:45:11 -0400

    feat(dmsquash-live,overlayfs): use rootfs-block/mount-root.sh
    Mount the overlay bearing partition once using rootfs-block/
      mount-root.sh to take advantage of its established features.
    Refactor setting of filesystem options for dmsquash-live.

--- modules.d/70overlayfs/module-setup.sh
+++ modules.d/70overlayfs/module-setup.sh
@@ -15,6 +15,9 @@ installkernel() {
 
 install() {
     dracut_module_included dmsquash-live || {
+        if dracut_module_included "systemd-initrd"; then
+            inst_script "$moddir/overlayfs-generator.sh" "$systemdutildir"/system-generators/dracut-overlayfs-generator
+        fi
         inst_hook cmdline 30 "$moddir/parse-overlayfs.sh"
         inst_hook pre-udev 30 "$moddir/overlayfs-genrules.sh"
         inst_script "$moddir/root-overlayfs.sh" "/sbin/root-overlayfs"
@@ -25,7 +28,7 @@ install() {
     inst_hook pre-mount 01 "$moddir/prepare-overlayfs.sh"
     inst_hook mount 01 "$moddir/mount-overlayfs.sh"     # overlay on top of block device
     if dracut_module_included "systemd-initrd"; then
-        inst_script "$moddir/overlayfs-generator.sh"
+        inst_script "$moddir/../74rootfs-block/mount-root.sh" "/sbin/mount-root"
     else
         inst_hook pre-pivot 10 "$moddir/mount-overlayfs.sh" # overlay on top of network device (e.g. nfs)
     fi
