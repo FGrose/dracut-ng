6dc75f2c31f2961acb5982c2f6beba6b09586898
Frederick Grose <fgrose@sugarlabs.org>
2024-10-09 20:56:19 -0400

    feat(<devspec> handling): specify by ID_SERIAL_SHORT
    Allow a target disc to be specified by its ID_SERIAL_SHORT value.
    Provide functions to identify discs by ID_SERIAL_SHORT and
      mask any commas in that value.
    Expand label_uuid_to_dev() and label_uuid_udevadm_trigger()
      to handle serial=<ID_SERIAL_SHORT>/serial/<devspec> as input.
    
    This permits specification of a disk device by its normally
      unchangeable identifier.

--- modules.d/70partition-lib/partition-lib.sh
+++ modules.d/70partition-lib/partition-lib.sh
@@ -834,11 +834,35 @@ parse_cfgArgs() {
                     [ "$p_Partition" ] && removePt="$p_Partition"
                 }
                 ;;
+            serial=?*)
+                ISS=${1%%/serial/*}
+                diskDevice=$(ID_SERIAL_SHORT_to_disc "${ISS#serial=}")
+                echo "$diskDevice" > /run/initramfs/diskdev
+                get_partitionTable "$diskDevice"
+                ptSpec=${1#*/serial/}
+                [ "$ptSpec" ] && {
+                    case "$ptSpec" in
+                        *[!0-9]* | 0*)
+                            # Anything but a positive integer:
+                            p_Partition=$(label_uuid_to_dev "$ptSpec")
+                            ;;
+                        *)
+                            p_Partition=$(aptPartitionName "$diskDevice" "$partNbr")
+                            ;;
+                    esac
+                }
+                ;;
             ea=?*)
                 extra_attrs="${*}"
                 extra_attrs=${extra_attrs#ea=}
                 break
                 # ea,extra attribute,s must be the final arguments.
+                ;;
+            PROMPTDK | PROMPTPT)
+                prompt_for_device "${1#PROMPT}"
+                ;;
+            PROMPTDR)
+                prompt_for_path "$1"
                 ;;
             PROMPTSZ)
                 # Assigns sizeGiB.
