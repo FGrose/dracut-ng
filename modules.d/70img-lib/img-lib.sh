9c23753f2a9c08213d4ad84a2fdc0c4de501c1be
Frederick Grose <fgrose@sugarlabs.org>
2024-10-19 14:44:11 -0400

    fix(check_live_ram): return warning if size could not be determined
    Return a warning if the memory size was not determined, as when
      /proc/meminfo is unpopulated early in boot.
    
    This avoids a syntax error in the subsequent calculation of memsize.

--- modules.d/70img-lib/img-lib.sh
+++ modules.d/70img-lib/img-lib.sh
@@ -99,19 +99,22 @@ unpack_img() {
 # Increase /run tmpfs size, if needed.
 check_live_ram() {
     local minmem imgsize memsize runsize runavail
-    minmem=$(getarg rd.minmem)
+    [ "$1" ] || {
+        warn "Image size could not be determined."
+        return 0
+    }
+    memsize=$(check_meminfo MemTotal:) || {
+        warn "Memory size could not be determined."
+        return 0
+    }
     imgsize=$1
-    memsize=$(($(check_meminfo MemTotal:) >> 10))
+    minmem=$(getarg rd.minmem)
+    memsize=$((${memsize:-1} >> 10))
     # shellcheck disable=SC2046
     set -- $(findmnt -bnro SIZE,AVAIL /run)
     # bytes to MiB
     runsize=$(($1 >> 20))
     runavail=$(($2 >> 20))
-
-    [ "$imgsize" ] || {
-        warn "Image size could not be determined"
-        return 0
-    }
 
     if [ $((memsize - imgsize)) -lt "${minmem:=1024}" ]; then
         sed -i "N;/and attach it to a bug report./s/echo$/echo\n\
