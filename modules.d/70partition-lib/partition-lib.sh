875b896b2d2a2a2e52b1f36bf7f58e5874c2c1fb
Frederick Grose <fgrose@sugarlabs.org>
2024-10-31 16:52:15 -0400

    feat(dmsquash-live): enable specifying a new overlay directory
    Use rd.live.dir=PROMPT to invoke an interactive dialog to specify
      the new directory name for OverlayFS persistence on the
      rd.overlay=<devspec> partition.
    Use rd.overlay= parameter argumemts
      - new_pt_for:<live_dir> for creation of a new persistence
        partition for an overlay of the <live_dir> base image.
        Omit <devspec> to partition on the existing disk, or use
        the PROMPT__ arguments below for further directions.
      - PROMPTDK to invoke a console menu to select a disk device.
     (- PROMPTFS has already be introduced to invoke a menu to
        specify the filesystem type for the new persistence partition.
     (- PROMPTSZ has already been introduced to interactively set
        a new persistence partition size.)
    Update the GRUB configuration file to show menuentries for the new
      boot target & its alternative overlay directory options.

--- modules.d/70partition-lib/partition-lib.sh
+++ modules.d/70partition-lib/partition-lib.sh
@@ -441,8 +441,6 @@ prompt_for_path() {
         obj="${obj%%[\`\'|
 ]*}"
     }
-    prompt_for_input
-}
 
 # Prompt for Live directory name
 prompt_for_livedir() {
@@ -666,6 +664,7 @@ parse_cfgArgs() {
                 ISS=${1%%/serial/*}
                 diskDevice=$(ID_SERIAL_SHORT_to_disc "${ISS#serial=}")
                 echo "$diskDevice" > /run/initramfs/diskdev
+                get_partitionTable "$diskDevice"
                 ptSpec=${1#*/serial/}
                 [ "$ptSpec" ] && {
                     case "$ptSpec" in
@@ -679,10 +678,6 @@ parse_cfgArgs() {
                     esac
                 }
                 ;;
-            auto)
-                espStart=1
-                cfg=ovl
-                ;;
             esp=*)
                 szESP=${1#esp=}
                 espStart=1
@@ -692,6 +687,13 @@ parse_cfgArgs() {
                 extra_attrs=${extra_attrs#ea=}
                 break
                 # ea,extra attribute,s must be the final arguments.
+                ;;
+            new_pt_for:*)
+                # New overlay partition for an existing ovl_dir:
+                base_dir="${1##*:}"
+                cfg=ovl:"${1%:*}"
+                # Trigger default ovlpath specification.
+                rd_overlay=''
                 ;;
             PROMPTDK | PROMPTPT)
                 prompt_for_device "${1#PROMPT}"
@@ -779,7 +781,11 @@ prep_Partition() {
             freeSpaceStart=${2%B}
             ;;
     esac
-    [ "$removePt" ] || freeSpaceStart=$((${3%B} + 1))
+    [ "$removePt" ] || {
+        freeSpaceStart=$((${3%B} + 1))
+        # dd'd .iso size
+        sz=$((freeSpaceStart + 32768))
+    }
     byteMax=$((szDisk - 268435456))
 
     # Make optimalIO alignment at least 4 MiB.
@@ -815,22 +821,10 @@ prep_Partition() {
     sizeGiB=${sizeGiB:+$((sizeGiB << 30))}
     partitionEnd="$((partitionStart + ${sizeGiB:-$szDisk} - 512))"
     [ "$partitionEnd" -gt "$freeSpaceEnd" ] && partitionEnd="$freeSpaceEnd"
+    p_ptCmd="--align optimal mkpart ${live_dir}.. ${partitionStart}B ${partitionEnd}B"
 
     run_parted "$diskDevice" ${removePtNbr:+rm $removePtNbr} \
         "${newptCmd:=--align optimal mkpart LiveOS_persist "${partitionStart}B" "${partitionEnd}B"}"
-
-    # shellcheck disable=SC2086
-    run_parted "${diskDevice}" --fix \
-        ${removePtNbr:+rm "$removePtNbr"} \
-        ${espCmd:+$espCmd} \
-        ${newptCmd:=--align optimal mkpart "$live_dir".. "${partitionStart}"B "${partitionEnd}"B}
-    : "${cfg:=ovl}"
-
-    [ "$espCmd" ] && {
-        udevadm trigger --name-match "$ESP" --action add --settle > /dev/kmsg 2>&1
-        mkfs_config fat ESP $((partitionStart - espStart))
-        create_Filesystem fat "$ESP"
-    }
 
     newptCmd="$p_ptCmd"
     # LiveOS persistence partition type
@@ -843,6 +837,12 @@ prep_Partition() {
         ${newptCmd}
     : "${cfg:=ovl}"
 
+    [ "$espCmd" ] && {
+        udevadm trigger --name-match "$ESP" --action add --settle > /dev/kmsg 2>&1
+        mkfs_config fat ESP $((partitionStart - espStart))
+        create_Filesystem fat "$ESP"
+    }
+
     # Set new partition type with command - $@
     set_pt_type() {
         get_partitionTable "$diskDevice"
@@ -853,12 +853,15 @@ prep_Partition() {
     # shellcheck disable=SC2086
     set_pt_type $newptCmd
 
-    p_Partition=$(aptPartitionName "$diskDevice" "$newPtNbr")
-    udevadm trigger --name-match "$p_Partition" --action add --settle > /dev/kmsg 2>&1
-    ln -sf "$p_Partition" /run/initramfs/p_pt
+    [ "$p_Partition" ] || {
+        p_Partition=$(aptPartitionName "$diskDevice" "$newptNbr")
 
-    [ "$p_ptFlags" ] || set_FS_opts_w "${fsType:-ext4}" p_ptFlags
-    mkfs_config "${p_ptfsType:=ext4}" LiveOS_persist $((partitionEnd - partitionStart + 1)) "${extra_attrs}"
-    wipefs --lock -af${QUIET:+q} "$p_Partition"
-    create_Filesystem "$p_ptfsType" "$p_Partition"
+        udevadm trigger --name-match "$p_Partition" --action add --settle > /dev/kmsg 2>&1
+        ln -sf "$p_Partition" /run/initramfs/p_pt
+
+        [ "$p_ptFlags" ] || set_FS_opts_w "${fsType:-ext4}" p_ptFlags
+        mkfs_config "${p_ptfsType:=ext4}" LiveOS_persist $((partitionEnd - partitionStart + 1)) "${extra_attrs}"
+        wipefs --lock -af${QUIET:+q} "$p_Partition"
+        create_Filesystem "$p_ptfsType" "$p_Partition"
+    }
 }
