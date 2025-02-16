6df71b643c4afe7a60daaec08c649975b51e7acb
Frederick Grose <fgrose@sugarlabs.org>
2025-02-16 18:02:26 -0500

    feat(overlayfs): decouple dmsquash-live & non-live OverlayFS boots
    Use root=ovl:<device specification> to call for a OverlayFS
      mount of a non-live root filesystem partition.
    Use rd.overlayfs={[1|<source_name>][,<devspec>[:<pathspec>]][,subvol=<subvol_path>]|0|no|off}
      for specifying an optional name for the OverlayFS mount source
      (default=os_rootfs), or an optional persistence device for the
      OverlayFS mount.
    Accommodate space, hyphen, slash in the mount source name.
    Provide a systemd mount unit with a dependency on a corresponding
      systemd-fsck@.service for a persistent overlay partition.
    Use rd.ovl.dir= for setting the pathspec parent directory within
      /RootOverlays/ (default=RootOvl).
    Use rd.ovl.flags=<mount options> to specify additional mount options
      for the partition bearing the OverlayFS overlay for a filesystem.
    Deprecate rd.live.overlay.reset for rd.overlayfs.reset.
    Merge livenet-generator into dmsquash-generator and consolidate
      overlayfs-generator & dmsquash-generator with functions in
      overlayfs-lib.sh to share preparation & mounting with dmsquash-live
      OverlayFS overlays.
    Support btrfs subvolumes and snapshots for root filesystem or
      persistent overlays.
    Allow rd.live.dir= or rd.ovl.dir= to specify creation of a new
      OverlayFS overlay directory.
    Adjust documentation from root=<path to blockdevice> to
      root=<device specification> to be properly inclusive.
    Document EROFS as an alternative for SquashFS images.

---
+++ modules.d/70overlayfs/root-overlayfs.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+[ "$RD_DEBUG" = yes ] && set -x
+if [ "$BASH" ]; then
+    PS4='+ $(IFS=" " read -r u0 _ </proc/uptime; echo "$u0") $BASH_SOURCE@$LINENO ${FUNCNAME:+$FUNCNAME()}: '
+else
+    PS4='+ $0@$LINENO: '
+fi
+command -v getarg > /dev/null || . /lib/dracut-lib.sh
+command -v do_overlayfs > /dev/null || . /lib/overlayfs-lib.sh
+
+PATH=/usr/sbin:/usr/bin:/sbin:/bin
+
+[ "$1" ] || exit 1
+ovl_pt=$(getarg rd.overlayfs) || exit 0
+load_fstype overlay || die 'OverlayFS is required but unavailable.'
+
+root_pt="$1"
+get_ovl_pt os_rootfs ovl_pt
+[ "$ovl_pt" = off ] && exit 0
+
+strstr "$ovl_pt" ":" && ovlpath=${ovl_pt##*:}
+
+# shellcheck disable=SC2046
+devInfo="
+$(blkid --probe --match-tag UUID -s LABEL -s TYPE --output export --usages filesystem "$root_pt")
+"
+# Works for block devices or image files.
+# missing tags will be skipped making order inconsistent between partitions.
+root_ptfsType="${devInfo#*
+TYPE=}"
+root_ptfsType="${root_ptfsType%%
+*}"
+# Retrieve UUID, or if not present, PARTUUID.
+uuid="${devInfo#*[
+_]UUID=}"
+uuid="${uuid%%
+*}"
+label="${devInfo#*
+LABEL=}"
+label="${label%%
+*}"
+
+[ "$volatile" ] || {
+    [ -b "$ovl_pt" ] || Die "$ovl_pt is not available."
+
+    # Add a mount and directories for OverlayFS persistent writes.
+    mntDir=/run/os_persist
+    ovl_dir=$(getarg rd.ovl.dir) || ovl_dir=RootOvl
+    # Place overlays in a standard directory.
+    ovl_dir=RootOverlays/"$ovl_dir"
+    do_overlayfs
+}
+
+ln -sf "$root_pt" /run/initramfs/rorootfs
+fstype="${root_ptfsType:-auto}" srcPartition="$root_pt" \
+    mountPoint=/run/rootfsbase srcflags="$rflags",ro \
+    fsckoptions="$fsckoptions" mount_partition
+ln -sf "$(findmnt -no SOURCE /run/rootfsbase)" /run/initramfs/rorootfs
+
+ln -s null /dev/root
+
+exit 0
