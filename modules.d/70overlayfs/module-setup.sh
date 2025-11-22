febe1738ed1675e6b6303fbd45076e352db6834f
Frederick Grose <fgrose@sugarlabs.org>
2025-11-21 20:13:38 -0500

    fix(overlayfs): adjust SELinux context type for OverlayFS directories
    Add a systemd service to change the SELinux context type to root_t for
      OverlayFS directories on virtual filesystems shortly after switching
      root.
    Use chcon in a pre-pivot script to adjust the context in non-virtual
      filesystems.
    
    Fixes #1042 for systems using systemd as init after switch-root.

--- modules.d/70overlayfs/module-setup.sh
+++ modules.d/70overlayfs/module-setup.sh
@@ -14,9 +14,13 @@ installkernel() {
 }
 
 install() {
-    inst_hook pre-mount 01 "$moddir/prepare-overlayfs.sh"
-    inst_hook mount 01 "$moddir/mount-overlayfs.sh"       # overlay on top of block device
-    inst_hook pre-pivot 00 "$moddir/prepare-overlayfs.sh" # prepare for network root (e.g. nfs)
-    inst_hook pre-pivot 10 "$moddir/mount-overlayfs.sh"   # overlay on top of network device
     inst_simple "$moddir/overlayfs-lib.sh" "/lib/overlayfs-lib.sh"
+    inst_hook pre-mount 01 "$moddir/prepare-overlayfs.sh"
+    dracut_module_included systemd || {
+        inst_hook mount 01 "$moddir/mount-overlayfs.sh"     # overlay on top of block device
+    }
+    dracut_module_included net-lib && inst_hook pre-pivot 10 "$moddir/mount-overlayfs.sh" # overlay on top of network device (e.g. nfs)
+    inst_hook pre-pivot 51 "$moddir/overlayfs-pre-pivot-actions.sh"
+    inst_script "$moddir"/overlayfs-root_t.sh /sbin/overlayfs-root_t.sh
+    inst_simple "$moddir"/overlayfs-root_t.service "$systemdsystemunitdir"/overlayfs-root_t.service
 }
