155a7681974e2febfd3dff66867b77cb2d9cf74f
Frederick Grose <fgrose@sugarlabs.org>
2025-11-27 08:24:42 -0500

    refactor(dmsquash-live apply-live-updates): drop /run bind mount
    Drop the bind mount of /run to $NEWROOT/run as /run is mount --move(d)
      there at the end of init.
    Also move a comment line to just ahead of its effective code line.

--- modules.d/70dmsquash-live/apply-live-updates.sh
+++ modules.d/70dmsquash-live/apply-live-updates.sh
@@ -2,17 +2,15 @@
 
 if [ -h /dev/root ] && [ -d /run/initramfs/live/updates ] || [ -d /updates ]; then
     info "Applying updates to live image..."
-    mount -o bind /run "$NEWROOT"/run
-    # avoid overwriting symlinks (e.g. /lib -> /usr/lib) with directories
     for d in /updates /run/initramfs/live/updates; do
         [ -d "$d" ] || continue
         (
             cd "$d" || return 0
+            # avoid overwriting symlinks (e.g., /lib -> /usr/lib) with directories
             find . -depth -type d -exec mkdir -p "$NEWROOT/{}" \;
             find . -depth \! -type d -exec cp -a "{}" "$NEWROOT/{}" \;
         )
     done
-    umount "$NEWROOT"/run
 fi
 # release resources on iso-scan boots with rd.live.ram
 if [ -d /run/initramfs/isoscan ] \
