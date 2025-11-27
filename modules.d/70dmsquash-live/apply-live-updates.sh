f3a26c8f74793edf134edf7aee0f505ed6e217d9
Frederick Grose <fgrose@sugarlabs.org>
2025-11-27 08:24:42 -0500

    refactor(dmsquash-live apply-live-updates): rescope /run bind mount
    Bind mount /run to $NEWROOT/run for the whole script & document reason.
    (/run is mounted at $NEWROOT/run after switch_root;
     bind-mount it in place so that updates for /run actually land in /run.)
    
    Also move a document line to just ahead of it's effective code line.

--- modules.d/70dmsquash-live/apply-live-updates.sh
+++ modules.d/70dmsquash-live/apply-live-updates.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+# /run is mounted at $NEWROOT/run after switch_root;
+# bind-mount it in place so that updates for /run actually land in /run.
+mount -o bind /run "$NEWROOT"/run
+
 if [ -h /dev/root ] && [ -d /run/initramfs/live/updates ] || [ -d /updates ]; then
     info "Applying updates to live image..."
-    mount -o bind /run "$NEWROOT"/run
-    # avoid overwriting symlinks (e.g. /lib -> /usr/lib) with directories
     for d in /updates /run/initramfs/live/updates; do
         [ -d "$d" ] || continue
         (
             cd "$d" || return 0
+            # avoid overwriting symlinks (e.g., /lib -> /usr/lib) with directories
             find . -depth -type d -exec mkdir -p "$NEWROOT/{}" \;
             find . -depth \! -type d -exec cp -a "{}" "$NEWROOT/{}" \;
         )
     done
-    umount "$NEWROOT"/run
 fi
 # release resources on iso-scan boots with rd.live.ram
 if [ -d /run/initramfs/isoscan ] \
@@ -20,3 +22,6 @@ if [ -d /run/initramfs/isoscan ] \
     umount --detach-loop /run/initramfs/live
     umount /run/initramfs/isoscan
 fi
+
+umount "$NEWROOT"/run
+
