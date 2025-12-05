1425b1d07f82ef5fc8ea2a9b7a42075fb8863a24
Frederick Grose <fgrose@sugarlabs.org>
2025-12-05 18:49:48 -0500

    doc(dmsquash-live,overlayfs): *-pre-pivot-actions.sh in code comments
    Document some of the code.

--- modules.d/70overlayfs/overlayfs-pre-pivot-actions.sh
+++ modules.d/70overlayfs/overlayfs-pre-pivot-actions.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
-getargbool rd.overlay || return 0
+getargbool 0 rd.overlay || return 0
+
+# /run is mounted at $NEWROOT/run after switch_root;
+# bind-mount it in place so that updates for /run actually land in /run.
+mount -o bind /run "$NEWROOT"/run
 
 if [ -d /run/ovl/upperdir ]; then
     # Setup service for post switch-root relabelling of virtual filesystem objects.
@@ -18,3 +22,5 @@ fi
 
 # Hide the base rootfs mountpoint.
 umount -l /run/rootfsbase
+
+umount "$NEWROOT"/run
