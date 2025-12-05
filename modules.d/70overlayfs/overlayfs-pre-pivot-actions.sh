292f1dfd82be9ccb1bcb2708a57687088d7c4987
Frederick Grose <fgrose@sugarlabs.org>
2025-12-05 18:49:48 -0500

    doc(dmsquash-live,overlayfs): *-pre-pivot-actions.sh in code comments
    Document some of the code.

--- modules.d/70overlayfs/overlayfs-pre-pivot-actions.sh
+++ modules.d/70overlayfs/overlayfs-pre-pivot-actions.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
-getargbool rd.overlay || return 0
+getargbool 0 rd.overlay || return 0
+
+# /run is mounted at $NEWROOT/run after switch_root;
+# bind-mount it in place so that updates for /run actually land in /run.
+mount -o bind /run "$NEWROOT"/run
 
 if [ -h /run/overlayfs ]; then
     # Change SELinux context type for OverlayFS directories on non-virtual filesystems.
     PATH=/run/rootfsbase/usr/bin:/run/rootfsbase/usr/sbin:/run/rootfsbase/bin:/run/rootfsbase/sbin:$PATH
     chcon system_u:object_r:root_t:s0 /run/overlayfs /run/ovlwork
 else
-    # Change SELinux context type for OverlayFS directories on virtual filesystems.
+    # Prepare systemd service to change SELinux contexts for OverlayFS upper directories in the virtual /run filesystem.
     cp /usr/lib/systemd/system/overlayfs-root_t.service "$NEWROOT"/usr/lib/systemd/system/overlayfs-root_t.service
     cp /usr/bin/overlayfs-root_t.sh "$NEWROOT"/usr/bin/overlayfs-root_t.sh
     mkdir "$NEWROOT"/usr/lib/systemd/system/local-fs-pre.target.wants
@@ -18,3 +22,5 @@ fi
 
 # Hide the base rootfs mountpoint.
 umount -l /run/rootfsbase
+
+umount "$NEWROOT"/run
