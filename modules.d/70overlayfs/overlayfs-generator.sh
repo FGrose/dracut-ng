f1b60e8ebbaecc15616b11923e1b1851fd253456
Frederick Grose <fgrose@sugarlabs.org>
2025-12-05 21:14:40 -0500

    feat(overlayfs,dmsquash-live,livenet): enable volatile OverlayFS mount
    Mount the OverlayFS with the volatile option if the overlay is in a
    volatile filesystem.
    
    This avoids all forms of sync calls to the upper filesystem.

--- modules.d/70overlayfs/overlayfs-generator.sh
+++ modules.d/70overlayfs/overlayfs-generator.sh
@@ -7,11 +7,13 @@ command -v getarg > /dev/null || . /lib/dracut-lib.sh
 OverlayFS="$(getarg rd.overlayfs)" || exit 0
 load_fstype overlay || die 'OverlayFS is required but unavailable.'
 
+volatile=volatile
 case "${OverlayFS%%[=/]*}" in
     0 | no | off) exit 0 ;;
     '' | 1) ovlfs_name=os_rootfs ;;
     "${OverlayFS%%,*}") ovlfs_name=${OverlayFS%%,*} ;;
     *) # devspec present
+        unset -v 'volatile'
         # with source name prefix
         [ "${OverlayFS%%,*}" != "$OverlayFS" ] && ovlfs_name=${OverlayFS%%,*}
         ;;
@@ -38,10 +40,13 @@ GENERATOR_DIR="$2"
     echo Before=initrd-root-fs.target
     echo [Mount]
     echo Where=/sysroot
-    getargbool 0 rd.overlayfs.readonly && readonly_overlay=--readonly
+    getargbool 0 rd.overlayfs.readonly && {
+        readonly_overlay=--readonly
+        volatile=volatile
+    }
     basedirs=lowerdir="${readonly_overlay:+/run/overlayfs-r:}"/run/rootfsbase
     echo What="${ovlfs_name:=os_rootfs}"
-    echo Options="${basedirs}",upperdir=/run/overlayfs,workdir=/run/ovlwork
+    echo Options="${volatile:+volatile,}${basedirs}",upperdir=/run/overlayfs,workdir=/run/ovlwork
     echo Type=overlay
 } > "$GENERATOR_DIR"/sysroot.mount
 ovlfs_name=$(echo "$ovlfs_name" | sed 's,/,\\x2f,g;s, ,\\x20,g;s,-,\\x2d,g;')
