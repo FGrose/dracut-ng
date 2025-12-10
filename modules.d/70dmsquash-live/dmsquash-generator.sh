9f44c7be96c743fe8ae828336cc911894769f144
Frederick Grose <fgrose@sugarlabs.org>
2025-12-09 19:31:32 -0500

    refactor(dmsquash-generator): reformat to support syntax highlighting
    Quote variables without adjacent static text to hightlight variables.
    Provide recommended generator name and SourcePath at top of file.
    Prefer || logic to [ -z as a shorter, more direct syntax for testing
      unset variables.

--- modules.d/70dmsquash-live/dmsquash-generator.sh
+++ modules.d/70dmsquash-live/dmsquash-generator.sh
@@ -3,19 +3,15 @@
 
 command -v get_rd_overlay > /dev/null || . /lib/overlayfs-lib.sh
 
-[ -z "$root" ] && root=$(getarg root=)
-
+[ "$root" ] || root=$(getarg root=)
 # support legacy syntax of passing rd.live.image and then just the base root
 if getargbool 0 rd.live.image; then
-    liveroot="live:$root"
+    liveroot=live:"$root"
 fi
-
-if [ "${root%%:*}" = "live" ]; then
+if [ "${root%%:*}" = live ]; then
     liveroot=$root
 fi
-
-[ "${liveroot%%:*}" = "live" ] || exit 0
-
+[ "${liveroot%%:*}" = live ] || exit 0
 case "$liveroot" in
     live:LABEL=* | LABEL=* | live:UUID=* | UUID=* | live:PARTUUID=* | PARTUUID=* | live:PARTLABEL=* | PARTLABEL=*)
         root="live:$(label_uuid_to_dev "${root#live:}")"
@@ -24,12 +20,12 @@ case "$liveroot" in
     live:CDLABEL=* | CDLABEL=*)
         root="${root#live:}"
         root="$(echo "$root" | sed 's,/,\\x2f,g;s, ,\\x20,g')"
-        root="live:/dev/disk/by-label/${root#CDLABEL=}"
+        root=live:/dev/disk/by-label/"${root#CDLABEL=}"
         rootok=1
         ;;
     live:/*.[Ii][Ss][Oo] | /*.[Ii][Ss][Oo])
         root="${root#live:}"
-        root="liveiso:${root}"
+        root=liveiso:"${root}"
         rootok=1
         ;;
     live:/dev/*)
@@ -70,7 +66,7 @@ getarg live.updates= && {
 [ "$rootok" ] || exit 0
 
 GENERATOR_DIR="$2"
-[ -z "$GENERATOR_DIR" ] && exit 1
+[ "$GENERATOR_DIR" ] || exit 1
 [ -d "$GENERATOR_DIR" ] || mkdir -p "$GENERATOR_DIR"
 
 rfstype="$(getarg rootfstype=)"
