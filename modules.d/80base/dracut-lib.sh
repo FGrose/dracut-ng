e37d46b5cef162a8ceb470e3cad2eb20d492ff1b
Frederick Grose <fgrose@sugarlabs.org>
2026-06-05 10:58:43 -0400

    feat(strstr): allow testing of empty string
    Do not return OK (true) if $1 is empty.

--- modules.d/80base/dracut-lib.sh
+++ modules.d/80base/dracut-lib.sh
@@ -33,9 +33,9 @@ escape() {
     printf '%s' "$1" | sed -e "s/'/'\\\\''/g"
 }
 
-# returns OK if $1 contains literal string $2 (and isn't empty)
+# returns OK if $1 contains literal string $2
 strstr() {
-    [ "${1##*"$2"*}" != "$1" ]
+    [ "a${1##*"$2"*}" != "a$1" ]
 }
 
 # returns OK if $1 matches (completely) glob pattern $2
